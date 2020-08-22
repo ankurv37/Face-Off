bbf03a80d9c56e2491982ee43ec302181e64ab6a
Ankur <ankurv37@gmail.com>
2020-08-22 15:04:26 -0400

    React camera capture

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+import Image from './Image/Image';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I am react app</h1>
+        <h2>Hi, I am react app 2</h2>
+        <Person />
+        <Image/ >
       </div>
     );
+  //return React.createElement('div',null,'hi','i am a react app !!');
   }
 }
 
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+const person = () => {
+  return <p>I' am a person!</p>
+};
+
+export default person;
