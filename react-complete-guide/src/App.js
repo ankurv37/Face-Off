1e5c976dbb2d11b54ce7d401ddfc933c260e630f
Ankur <ankurv37@gmail.com>
2020-09-08 22:36:23 -0400

    Sydney changes

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
-//import Image from './Image/Image';
+import Image from './Image/Image';
+import ScriptTag from 'react-script-tag';
+
 
 class App extends Component {
   state={
@@ -16,7 +18,9 @@ class App extends Component {
 switchNameHandler = (newName) =>{
   // console.log('was clicked');
   this.setState({persons: [
-    {name: newName, age:28},
+    {name: newName, age:28,uvindex:7 , SunburnTime:20 ,o3Level:25 ,underEyeWrinkles:3,
+       foreheadWrinkles:2 ,acne: 2, darkCircles: 4, skinage: 3 , elasticity: 3, firmness : 2,
+    CautionaryStatement: 'Seek shade between 10AM and 4PM when the sun’s rays are its strongest. Wearing protective clothing, including a hat and sunglasses, is a great way to limit exposure. Sunscreen should be applied every two hours, even on cloudy days, and reapplied after swimming or sweating.'},
     {name: 'Manu', age:31},
     {name: 'Steph', age:27}
   ]
@@ -26,21 +30,26 @@ switchNameHandler = (newName) =>{
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am react app</  h1>
-        <h2>Hi, I am react app 2</h2>
-        <button onClick={this.switchNameHandler.bind(this, 'Max!')}>Click </button>
+        <h1>Hi, I am Sydney Face AI</  h1>
+      <Image />
+        <button onClick={this.switchNameHandler.bind(this, 'Max!')}>Get Results </button>
         <Person
         name={this.state.persons[0].name}
         age={this.state.persons[0].age}
-        click={this.switchNameHandler.bind(this, 'Ankur')} >and this is my Project</Person>
-        <Person
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age}/>
-        <Person
-        name={this.state.persons[2].name}
-        age={this.state.persons[2].age}/>
-
+        uvindex={this.state.persons[0].uvindex}
+        SunburnTime={this.state.persons[0].SunburnTime}
+        o3Level={this.state.persons[0].o3Level}
+          underEyeWrinkles={this.state.persons[0].underEyeWrinkles}
+            foreheadWrinkles={this.state.persons[0].foreheadWrinkles}
+              acne={this.state.persons[0].acne}
+                darkCircles={this.state.persons[0].darkCircles}
+                  skinage={this.state.persons[0].skinage}
+                  elasticity={this.state.persons[0].elasticity}
+                  firmness={this.state.persons[0].firmness}
+        CautionaryStatement={this.state.persons[0].CautionaryStatement}
+        click={this.switchNameHandler.bind(this, 'Ankur')} ></Person>
       </div>
+
     );
   //return React.createElement('div',null,'hi','i am a react app !!');
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,7 +3,12 @@ import React,{Component} from 'react';
 const person = (props) => {
   return(
       <div>
-        <p onClick={props.click}>I' am a {props.name} and I am {props.age}  years old</p>
+        <p onClick={props.click}>Image tags for  {props.age}  years old {props.name} are</p>
+          <p>underEyeWrinkles : {props.underEyeWrinkles} foreheadWrinkles : {props.foreheadWrinkles}  darkCircles : {props.darkCircles}</p>
+          <p>skinage : {props.skinage}  elasticity : {props.elasticity} firmness : {props.firmness}</p>
+          <p>UVINDEX : {props.uvindex} SunburnTime : {props.SunburnTime} O3- Ozone level : {props.o3Level}</p>
+          <p>CautionaryStatement : {props.CautionaryStatement}</p>
+          <p> Recommended Sydney Article : (Show Relevant article, Enroll Action plans)</p>
         <p>{props.children}</p>
       </div>
     )};
